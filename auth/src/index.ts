0c5b1aa6833eb019d0209af42a3fdd40ad704070
Imad Jerra <imad.jerra@ariv.ca>
2020-12-28 10:19:06 -0500

    Connect to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import 'express-async-errors';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from "./routes/current-user";
 import { signUpRouter } from "./routes/signup";
@@ -9,10 +10,10 @@ import { signOutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error";
 import { NotFoundError } from "./errors/not-found-error";
 
-const PORT = "3000"
+const PORT = "3000";
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use(currentUserRouter);
 app.use(signUpRouter);
@@ -25,6 +26,22 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('connected to mongodb');
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(PORT, () => {
+        console.log(`auth-api is running at https://localhost:${PORT}`);
+    });
+};
+
+start();
