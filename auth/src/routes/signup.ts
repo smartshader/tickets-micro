3c60d8ddedb380dd7f6e835a3dc9cda5623d1c3b
Imad Jerra <imad.jerra@ariv.ca>
2020-12-28 11:20:16 -0500

    Create user on signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
+import { StatusCodes } from "http-status-codes";
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
@@ -21,10 +22,17 @@ router.post('/api/users/signup', [
 
     const { email, password } = req.body;
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+        console.log('Email in use');
+        res.send({});
+    }
 
-    res.send({});
+    const user = User.build({email, password});
+    await user.save();
+
+    res.status(StatusCodes.CREATED)
+        .send(user);
 });
 
 export { router as signUpRouter };
